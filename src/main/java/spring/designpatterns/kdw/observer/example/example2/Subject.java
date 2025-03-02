9c1287f029f89f02b4e1744b5ba986c15a2a23ad
DongWooKim4343 <kd6904@naver.com>
2025-03-02 17:58:28 +0900

    add: Observable 사용 추가

--- src/main/java/spring/designpatterns/kdw/observer/example/example2/Subject.java
+++ src/main/java/spring/designpatterns/kdw/observer/example/example2/Subject.java
@@ -1,5 +1,7 @@
 package spring.designpatterns.kdw.observer.example.example2;
 
+import java.util.Observer;
+
 public interface Subject {
     public void registerObserver(Observer o);
     public void removeObserver(Observer o);
