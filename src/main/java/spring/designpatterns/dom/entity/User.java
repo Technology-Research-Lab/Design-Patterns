193ce7bc1da015ba097daeca266d3b12d3bb7cc8
Dom1046 <rkrkwnj1046@gmail.com>
2025-03-08 14:26:31 +0900

    add: 필요 의존성 추가=테스트, 데이터베이스(mysql), 레디스, 테이블 생성= User

---
+++ src/main/java/spring/designpatterns/dom/entity/User.java
@@ -0,0 +1,13 @@
+package spring.designpatterns.dom.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+    private String userName;
+    private String password;
+}
